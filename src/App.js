0425f0074696146c0bd52a01faba0ce4d0bab1aa
Marco <marco_trabajo@yahoo.com>
2022-03-20 21:25:50 +0100

    features section, create cards using json, map and props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './Component/Head/Header'
 import Home from './Component/Hero/Home'
 import './App.css'
+import Features from './Component/Features/Features'
 
 const App = () => {
   return (
     <div>
       <Header />
       <Home />
+      <Features />
     </div>
   )
 }
